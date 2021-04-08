9d60e1d055ee34d2b2fb96b1849b11a7d342dbc1
NFJC <57829982+NFJC-360@users.noreply.github.com>
2021-04-08 11:02:00 +0200

    hsv or rgb

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,22 +37,36 @@ namespace colorPiker
             this.clr_view = new System.Windows.Forms.Panel();
             this.clr_code = new System.Windows.Forms.Label();
             this.copy_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rec1 = new System.Windows.Forms.TextBox();
+            this.rec2 = new System.Windows.Forms.TextBox();
+            this.rec3 = new System.Windows.Forms.TextBox();
+            this.toRGB = new System.Windows.Forms.Button();
+            this.toHSV = new System.Windows.Forms.Button();
             cntClk = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.red_scale)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.green_scale)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.blue_scale)).BeginInit();
             this.SuspendLayout();
             // 
+            // cntClk
+            // 
+            cntClk.Enabled = true;
+            cntClk.Interval = 10;
+            cntClk.Tick += new System.EventHandler(this.cntClk_Tick);
+            // 
             // red_scale
             // 
             this.red_scale.AutoSize = false;
-            this.red_scale.BackColor = System.Drawing.Color.Red;
-            this.red_scale.Location = new System.Drawing.Point(9, 9);
+            this.red_scale.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.red_scale.Location = new System.Drawing.Point(18, 9);
             this.red_scale.Margin = new System.Windows.Forms.Padding(0);
-            this.red_scale.Maximum = 255;
+            this.red_scale.Maximum = 360;
             this.red_scale.Name = "red_scale";
             this.red_scale.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.red_scale.Size = new System.Drawing.Size(19, 226);
+            this.red_scale.Size = new System.Drawing.Size(19, 212);
             this.red_scale.SmallChange = 0;
             this.red_scale.TabIndex = 0;
             this.red_scale.TickStyle = System.Windows.Forms.TickStyle.None;
@@ -60,13 +74,13 @@ namespace colorPiker
             // green_scale
             // 
             this.green_scale.AutoSize = false;
-            this.green_scale.BackColor = System.Drawing.Color.Lime;
-            this.green_scale.Location = new System.Drawing.Point(51, 9);
+            this.green_scale.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.green_scale.Location = new System.Drawing.Point(63, 9);
             this.green_scale.Margin = new System.Windows.Forms.Padding(0);
-            this.green_scale.Maximum = 255;
+            this.green_scale.Maximum = 100;
             this.green_scale.Name = "green_scale";
             this.green_scale.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.green_scale.Size = new System.Drawing.Size(19, 226);
+            this.green_scale.Size = new System.Drawing.Size(19, 212);
             this.green_scale.SmallChange = 0;
             this.green_scale.TabIndex = 0;
             this.green_scale.TickStyle = System.Windows.Forms.TickStyle.None;
@@ -74,13 +88,13 @@ namespace colorPiker
             // blue_scale
             // 
             this.blue_scale.AutoSize = false;
-            this.blue_scale.BackColor = System.Drawing.Color.Blue;
-            this.blue_scale.Location = new System.Drawing.Point(100, 9);
+            this.blue_scale.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.blue_scale.Location = new System.Drawing.Point(108, 9);
             this.blue_scale.Margin = new System.Windows.Forms.Padding(0);
-            this.blue_scale.Maximum = 255;
+            this.blue_scale.Maximum = 100;
             this.blue_scale.Name = "blue_scale";
             this.blue_scale.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.blue_scale.Size = new System.Drawing.Size(19, 226);
+            this.blue_scale.Size = new System.Drawing.Size(19, 212);
             this.blue_scale.SmallChange = 0;
             this.blue_scale.TabIndex = 0;
             this.blue_scale.TickStyle = System.Windows.Forms.TickStyle.None;
@@ -93,46 +107,140 @@ namespace colorPiker
             this.clr_view.Size = new System.Drawing.Size(140, 140);
             this.clr_view.TabIndex = 1;
             // 
-            // cntClk
-            // 
-            cntClk.Enabled = true;
-            cntClk.Interval = 10;
-            cntClk.Tick += new System.EventHandler(this.cntClk_Tick);
-            // 
             // clr_code
             // 
             this.clr_code.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.clr_code.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.clr_code.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.clr_code.ForeColor = System.Drawing.Color.White;
-            this.clr_code.Location = new System.Drawing.Point(188, 166);
+            this.clr_code.Location = new System.Drawing.Point(208, 166);
             this.clr_code.Name = "clr_code";
-            this.clr_code.Size = new System.Drawing.Size(190, 23);
+            this.clr_code.Size = new System.Drawing.Size(108, 23);
             this.clr_code.TabIndex = 2;
-            this.clr_code.Text = "clr_code";
+            this.clr_code.Text = "#000000";
             this.clr_code.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // copy_btn
             // 
-            this.copy_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.copy_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
             this.copy_btn.FlatAppearance.BorderSize = 0;
             this.copy_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.copy_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.copy_btn.ForeColor = System.Drawing.Color.White;
-            this.copy_btn.Location = new System.Drawing.Point(246, 212);
+            this.copy_btn.Image = global::colorPiker.Properties.Resources.icons8_copy_24px;
+            this.copy_btn.Location = new System.Drawing.Point(322, 166);
             this.copy_btn.Name = "copy_btn";
-            this.copy_btn.Size = new System.Drawing.Size(60, 23);
+            this.copy_btn.Size = new System.Drawing.Size(31, 29);
             this.copy_btn.TabIndex = 3;
-            this.copy_btn.Text = "COPY";
             this.copy_btn.UseVisualStyleBackColor = false;
             this.copy_btn.Click += new System.EventHandler(this.copy_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "H";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(58, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "S";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(103, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "V";
+            // 
+            // rec1
+            // 
+            this.rec1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rec1.Location = new System.Drawing.Point(9, 282);
+            this.rec1.Name = "rec1";
+            this.rec1.Size = new System.Drawing.Size(37, 29);
+            this.rec1.TabIndex = 5;
+            this.rec1.TextChanged += new System.EventHandler(this.rec1_TextChanged);
+            // 
+            // rec2
+            // 
+            this.rec2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rec2.Location = new System.Drawing.Point(54, 282);
+            this.rec2.Name = "rec2";
+            this.rec2.Size = new System.Drawing.Size(37, 29);
+            this.rec2.TabIndex = 5;
+            this.rec2.TextChanged += new System.EventHandler(this.rec2_TextChanged);
+            // 
+            // rec3
+            // 
+            this.rec3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rec3.Location = new System.Drawing.Point(99, 282);
+            this.rec3.Name = "rec3";
+            this.rec3.Size = new System.Drawing.Size(37, 29);
+            this.rec3.TabIndex = 5;
+            this.rec3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // toRGB
+            // 
+            this.toRGB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(100)))));
+            this.toRGB.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.toRGB.FlatAppearance.BorderSize = 0;
+            this.toRGB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.toRGB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toRGB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.toRGB.Location = new System.Drawing.Point(229, 251);
+            this.toRGB.Name = "toRGB";
+            this.toRGB.Size = new System.Drawing.Size(49, 31);
+            this.toRGB.TabIndex = 6;
+            this.toRGB.Text = "RGB";
+            this.toRGB.UseVisualStyleBackColor = false;
+            this.toRGB.Click += new System.EventHandler(this.toRGB_Click);
+            // 
+            // toHSV
+            // 
+            this.toHSV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(15)))));
+            this.toHSV.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.toHSV.FlatAppearance.BorderSize = 0;
+            this.toHSV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.toHSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toHSV.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.toHSV.Location = new System.Drawing.Point(284, 251);
+            this.toHSV.Name = "toHSV";
+            this.toHSV.Size = new System.Drawing.Size(49, 31);
+            this.toHSV.TabIndex = 6;
+            this.toHSV.Text = "HSV";
+            this.toHSV.UseVisualStyleBackColor = false;
+            this.toHSV.Click += new System.EventHandler(this.toHSV_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
-            this.ClientSize = new System.Drawing.Size(418, 244);
+            this.ClientSize = new System.Drawing.Size(418, 315);
+            this.Controls.Add(this.toHSV);
+            this.Controls.Add(this.toRGB);
+            this.Controls.Add(this.rec3);
+            this.Controls.Add(this.rec2);
+            this.Controls.Add(this.rec1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.copy_btn);
             this.Controls.Add(this.clr_code);
             this.Controls.Add(this.clr_view);
@@ -148,6 +256,7 @@ namespace colorPiker
             ((System.ComponentModel.ISupportInitialize)(this.green_scale)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.blue_scale)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -159,6 +268,14 @@ namespace colorPiker
         private System.Windows.Forms.Panel clr_view;
         private System.Windows.Forms.Label clr_code;
         private System.Windows.Forms.Button copy_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox rec1;
+        private System.Windows.Forms.TextBox rec2;
+        private System.Windows.Forms.TextBox rec3;
+        private System.Windows.Forms.Button toRGB;
+        private System.Windows.Forms.Button toHSV;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,26 +12,242 @@ namespace colorPiker
 {
     public partial class Form1 : Form
     {
-        public string R, G, B;
+        public float C, X, M, R, G, B, H, S, V, H1, S1, V1;
+        public int R1, G1, B1;
+        public Boolean ClrScheeme1;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            ClrScheeme1 = true;
+
+        }
+
+        public float Max(float n1, float n2, float n3)
+        {
+            if (n1 > n2 && n1> n3)
+                return n1;
+
+            if (n2 > n1 && n2 > n3)
+                return n2;
+
+            if (n3 > n2 && n3 > n1)
+                return n3;
+
+            return 0;
+        }
+
+        public float Min(float n1, float n2, float n3)
+        {
+            if (n1 < n2 && n1 < n3)
+                return n1;
+
+            if (n2 < n1 && n2 < n3)
+                return n2;
+
+            if (n3 < n2 && n3 < n1)
+                return n3;
+
+            return 0;
+        }
+
+        public void cRtoH()
+        {
+
+            H1 = H / 255;
+            S1 = S / 255;
+            V1 = V / 255;
+            C = Max(H1,S1,V1);
+            X = Min(H1, S1, V1);
+            M = C - X;
+
+
+            if (M==0)
+                R = 0;
+            else if (C == H1)
+                R = (((S1 - V1) / M) % 6);
+            else if (C == S1)
+                R = (((V1 - H1) / M) + 2);
+            else if (C == V1)
+                R = (((H1 - S1) / M) + 4);
+
+
+            R *= 60;
+
+            if (C == 0)
+                G = 0;
+            else
+                G = (M / C)*100;
+
+            B = C*100;
+
+            R1 = (int)Math.Round(R);
+            G1 = (int)Math.Round(G);
+            B1 = (int)Math.Round(B);
+
+        }
+
+        public void cHtoR()
+        {
+
+            C = (V / 100) * (S / 100);
+            X = C * (1 - Math.Abs((((H / 60) % 2) - 1)));
+            M = (V / 100) - C;
+
+
+            if (0 <= H && H < 60)
+            {
+                R = C;
+                G = X;
+                B = 0;
+            }
+            else if (60 <= H && H < 120)
+            {
+                R = X;
+                G = C;
+                B = 0;
+            }
+            else if (120 <= H && H < 180)
+            {
+                R = 0;
+                G = C;
+                B = X;
+            }
+            else if (180 <= H && H < 240)
+            {
+                R = 0;
+                G = X;
+                B = C;
+            }
+            else if (240 <= H && H < 300)
+            {
+                R = X;
+                G = 0;
+                B = C;
+            }
+            else if (300 <= H && H <= 360)
+            {
+                R = C;
+                G = 0;
+                B = X;
+            }
+        }
 
         private void copy_btn_Click(object sender, EventArgs e)
         {
             Clipboard.SetText(clr_code.Text);
         }
 
-        public Form1()
+
+        private void toHSV_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+            toHSV.ForeColor = Color.FromArgb(200, 200, 200);
+            toHSV.BackColor = Color.FromArgb(200, 200, 200,15);
+            toRGB.BackColor = Color.FromArgb(80, 80, 80,100);
+            toRGB.ForeColor = Color.FromArgb(80, 80, 80);
+
+
+            cRtoH();
+            ClrScheeme1 = true;
+            
+
+            label1.Text = "H";
+            label2.Text = "S";
+            label3.Text = "V";
+
+            red_scale.Maximum = 360;
+            green_scale.Maximum = 100;
+            blue_scale.Maximum = 100;
+
+
+            red_scale.Value   = R1;
+            green_scale.Value = G1;
+            blue_scale.Value  = B1;
+        }
+
+        private void toRGB_Click(object sender, EventArgs e)
+        {
+            toRGB.ForeColor = Color.FromArgb(200, 200, 200);
+            toHSV.BackColor = Color.FromArgb(80, 80, 80, 100);
+            toRGB.BackColor = Color.FromArgb(200, 200, 200, 15);
+            toHSV.ForeColor = Color.FromArgb(80, 80, 80);
+
+
+            cHtoR();
+            ClrScheeme1 = false;
+            
+
+            label1.Text = "R";
+            label2.Text = "G";
+            label3.Text = "B";
+
+            red_scale.Maximum = 255;
+            green_scale.Maximum = 255;
+            blue_scale.Maximum = 255;
+
+            red_scale.Value = R1;
+            green_scale.Value = G1;
+            blue_scale.Value = B1;
+        }
+
+        private void rec1_TextChanged(object sender, EventArgs e)
+        {
+            if (rec1.Text != "")
+                red_scale.Value = Int32.Parse(rec1.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (rec3.Text != "")
+                blue_scale.Value = Int32.Parse(rec3.Text);
+        }
+
+        private void rec2_TextChanged(object sender, EventArgs e)
+        {
+            if (rec2.Text != "")
+                green_scale.Value = Int32.Parse(rec2.Text);
+        }
+
+        public void RGBise()
+        {
+            R1 = red_scale.Value;
+            G1 = green_scale.Value;
+            B1 = blue_scale.Value;
+        }
+
+
+        public void HSVise()
+        {
+            cHtoR();
+
+            R1 = (int)Math.Round((R + M) * 255);
+            G1 = (int)Math.Round((G + M) * 255);
+            B1 = (int)Math.Round((B + M) * 255);
 
         }
 
+
+        
+
         private void cntClk_Tick(object sender, EventArgs e)
         {
-            clr_view.BackColor = Color.FromArgb(red_scale.Value, green_scale.Value, blue_scale.Value);
-            R = red_scale.Value.ToString();
-            G = green_scale.Value.ToString();
-            B = blue_scale.Value.ToString();
-            clr_code.Text = "RGB(" + R + "," + G + "," + B + ")";
+            H = red_scale.Value;
+            S = green_scale.Value;
+            V = blue_scale.Value;
+
+            rec3.Text = V.ToString();
+            rec1.Text = H.ToString();
+            rec2.Text = S.ToString();
+
+            if (ClrScheeme1)
+                HSVise();
+            else
+                RGBise();
+
+            clr_view.BackColor = Color.FromArgb(R1, G1, B1);
+            clr_code.Text = "#" + R1.ToString("X2") + G1.ToString("X2") + B1.ToString("X2");
         }
     }
 }
